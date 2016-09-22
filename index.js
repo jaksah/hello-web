fc54e78cfa4391867800dc26f3583ab1586c0ae4
Jakob Sahlström <jakob.sahlstrom@gmail.com>
2016-09-22 18:54:16 +0200

    initial local web server

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+var server = app.listen(3000, function () {
+	console.log('The app is listening on http://localhost:3000');
+});
+
+app.get('/', function (req, res) {
+	res.send('Hello Web!');
+});
